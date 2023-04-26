cb75d46d1ffae495d47df3c04d04bcfe72b525b9
Jetroot <lorean700@gmail.com>
2023-04-26 13:41:04 -0800

    add: new components to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Container from "../components/Container";
 import Layout from "../components/Layouts"
 import Hero from "../components/Hero";
+import Features from "@/components/Features";
+import SocialMedia from "@/components/SocialMedia";
 
 export default function Home() {
     return (
         <Layout>
             <Container>
                 <Hero />
+                <Features />
+                <SocialMedia />
             </Container>
         </Layout>
     );
