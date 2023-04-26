88298e30853f81b064e703bc25b61ad6ce414f1b
Jetroot <lorean700@gmail.com>
2023-04-26 13:37:14 -0800

    add: data components

---
+++ data/Features.tsx
@@ -0,0 +1,28 @@
+export default [
+    {
+        name: "Save money",
+        icon: "/icons/features/money.svg",
+        description: "No more wasting money on ads that don’t convert",
+        url: "#",
+    },
+    {
+        name: "Better targeting",
+        icon: "/icons/features/targeting.svg",
+        description:
+            "Better targeting with a high precision based on factors such as demographics, behavior and interests.",
+        url: "#",
+    },
+    {
+        name: "Scaling",
+        icon: "/icons/features/scaling.svg",
+        description: "Scale your ads to 10x better at the right time.",
+        url: "#",
+    },
+    {
+        name: "Connect your ads",
+        icon: "/icons/features/easy.svg",
+        description:
+            "Connect your social media ads from platforms like Facebook, Google, Tiktok ...",
+        url: "#",
+    },
+];
