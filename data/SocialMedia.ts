2359175b95497994a2504bed503adb5352f0c77b
Jetroot <lorean700@gmail.com>
2023-04-26 13:55:53 -0800

    update: update social media data

--- data/SocialMedia.ts
+++ data/SocialMedia.ts
@@ -6,24 +6,24 @@ export default [
         icon: BsFacebook,
         company: "Facebook",
         color: "#1877F2",
-        description: "Scale your ads to 10x better at the right time",
+        description: "Scale your ads to 10x better on facebook",
     },
     {
         icon: SiTiktok,
         company: "Tiktok",
         color: "#69C9D0",
-        description: "Scale your ads to 10x better at the right time",
+        description: "Scale your ads to 10x better on tiktok",
     },
     {
         icon: BsInstagram,
         company: "Instagram",
         color: "#F58529",
-        description: "Scale your ads to 10x better at the right time",
+        description: "Scale your ads to 10x better on instagram",
     },
     {
         icon: BsSnapchat,
         company: "Snapchat",
         color: "#e7e306",
-        description: "Scale your ads to 10x better at the right time",
+        description: "Scale your ads to 10x better on snapchat",
     },
 ];
