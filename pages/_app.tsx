fc97ec41050c465c5f508ec5a88a74b806b24ad6
Jetroot <lorean700@gmail.com>
2023-04-27 18:36:46 -0800

    fix: change components layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,27 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
+import Layouts from "../components/Layouts";
+import { useRouter } from "next/router";
+
 export default function App({ Component, pageProps }: AppProps) {
+    const { route } = useRouter();
+
     return (
         <>
             <Head>
                 <link rel="shortcut icon" href="/favicon.png" />
                 <title>Vylyn - 10x better campaigns</title>
-                <meta property="description" content="10x better campaigns" />
+                <meta property="description" content="10x better campaigns using AI" />
             </Head>
-            <Component {...pageProps} />
+
+            {route === "/" ? (
+                <Component {...pageProps} />
+            ) : (
+                <Layouts>
+                    <Component {...pageProps} />
+                </Layouts>
+            )}
         </>
     );
 }
