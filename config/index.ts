2071381a5441d899abb9e00f28eabeb222fb2515
Jetroot <lorean700@gmail.com>
2023-05-13 20:43:52 -0800

    refactor: editing sign up and sign in component

--- config/index.ts
+++ config/index.ts
@@ -3,3 +3,7 @@ export const USER_KEY_LOCAL_STORAGE_NAME = "_us";
 
 // access token key name in cookies
 export const ACCESS_TOKEN_NAME_IN_COOKIES = "_at";
+
+// Sign in & Sign up Uri
+export const SIGN_IN_URI = "/sign-in";
+export const SIGN_UP_URI = "/sign-in";
