4a813c647456dd88d23d427a8652ada1da85b3f4
Jetroot <lorean700@gmail.com>
2023-05-23 07:53:57 +0000

    feat: add campaign api queries like createCampaign & getCampaigns

---
+++ pages/api/campaign.ts
@@ -0,0 +1,88 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getToken } from "next-auth/jwt";
+import NextCors from "nextjs-cors";
+
+import { ResponseStatusCodes } from "@/config";
+import { createNewCampaign, getCampaigns } from "@/services";
+
+export default async function createCampaign(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    // Nextjs-cors
+    await NextCors(req, res, {
+        // Options
+        methods: ["GET", "PUT", "POST", "DELETE"],
+        origin: process.env.NEXT_PUBLIC_APP_URL || "http://127.0.0.1:3000/",
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+
+    // verify the token
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+    // Invalid token
+    if (!token) {
+        res.status(ResponseStatusCodes.UNAUTHORIZED.status).json({
+            data: { msg: ResponseStatusCodes.UNAUTHORIZED.msg, success: false },
+        });
+
+        return;
+    }
+
+    // Get request method
+    const { method } = req;
+    const { p } = req.query;
+    // console.log("query", req.query);
+    // console.log("method", method);
+
+    // Create new campaign
+    if (method === "POST") {
+        // Get data
+        const { campaign_status, campaign_title, campaign_objective } =
+            req.body;
+
+        // check if data exists
+        if (!campaign_status && !campaign_title && !campaign_objective) {
+            res.status(ResponseStatusCodes.ERROR.status).json({
+                data: { msg: ResponseStatusCodes.ERROR.msg, success: false },
+            });
+
+            return;
+        }
+
+        // create new campaign
+        let isCampaignSaved = await createNewCampaign(
+            campaign_status,
+            campaign_title,
+            campaign_objective,
+            token.sub!
+        );
+
+        if (isCampaignSaved) {
+            res.status(ResponseStatusCodes.CREATED.status).json({
+                data: {
+                    msg: ResponseStatusCodes.CREATED.msg,
+                    success: true,
+                },
+            });
+
+            return;
+        }
+
+        res.status(ResponseStatusCodes.ERROR.status).json({
+            data: { msg: ResponseStatusCodes.ERROR.msg, success: false },
+        });
+    }
+
+    // Get campaigns
+    if (method === "GET") {
+        const campaigns = await getCampaigns(token.sub!, p);
+        // console.log('camps', campaigns)
+        res.status(ResponseStatusCodes.OK.status).json({
+            data: {
+                campaigns,
+            },
+        });
+        return;
+    }
+}
