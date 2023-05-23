73121df7a3bcc4e75684157a101b55d6e85a25ad
Jetroot <lorean700@gmail.com>
2023-05-23 08:09:47 +0000

    refactor: change configs

--- config/index.ts
+++ config/index.ts
@@ -1,9 +1,38 @@
-// user key name in local storage
-export const USER_KEY_LOCAL_STORAGE_NAME = "_us";
-
-// access token key name in cookies
-export const ACCESS_TOKEN_NAME_IN_COOKIES = "_at";
+import mongoose from "mongoose";
 
 // Sign in & Sign up Uri
 export const SIGN_IN_URI = "/sign-in";
 export const SIGN_UP_URI = "/sign-in";
+
+// database connection
+export const connectToDb = () => {
+    return mongoose.connect("mongodb://root:example@localhost:27017/");
+};
+
+// Custom response
+export const ResponseStatusCodes = {
+    OK: {
+        status: 200,
+        msg: "OK!",
+    },
+    CREATED: {
+        status: 201,
+        msg: "Created Successfully!",
+    },
+    BAD_REQUEST: {
+        status: 400,
+        msg: "Bad Request",
+    },
+    UNAUTHORIZED: {
+        status: 401,
+        msg: "Unauthorized request!",
+    },
+    ERROR: {
+        status: 500,
+        msg: "Something went wrong!",
+    },
+    USER_ALREADY_EXISTS: {
+        status: 600,
+        msg: "User Already Exists!",
+    },
+};
