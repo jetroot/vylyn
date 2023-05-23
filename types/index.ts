f6de7a3b03bd7a54ba9b0a2364f479b651ed95f9
Jetroot <lorean700@gmail.com>
2023-05-23 07:58:50 +0000

    refactor: change user & campaign types

---
+++ types/index.ts
@@ -0,0 +1,25 @@
+// User Object
+export type UserType = {
+    id: string | undefined;
+    email: string | undefined;
+    fullName: string | undefined;
+    avatarUrl: string | undefined;
+};
+
+// Campaign Status
+export enum CampaignStatus {
+    ACTIVE,
+    PAUSED,
+}
+
+// Campaign Objectives
+export enum CampaignObjectives {
+    REACH,
+    ENGAGEMENT,
+    FOLLOWERS,
+    TRAFFIC,
+    AWARENESS,
+    APP_INSTALL,
+    CONVERSION,
+    LEADS,
+}
